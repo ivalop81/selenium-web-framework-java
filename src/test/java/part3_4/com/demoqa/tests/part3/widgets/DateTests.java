5d7d54f9b594379dffa13f00faa8d9fb23e4d638
ivalop81 <ivalop81@gmail.com>
2025-07-18 07:51:32 -0300

    feat: implemented DatePickerMenuPage test
    Signed-off-by: ivalop81 <ivalop81@gmail.com>

--- src/test/java/part3_4/com/demoqa/tests/part3/widgets/DateTests.java
+++ src/test/java/part3_4/com/demoqa/tests/part3/widgets/DateTests.java
@@ -1,17 +1,30 @@
 package part3_4.com.demoqa.tests.part3.widgets;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
 
 public class DateTests extends BaseTest {
 
     @Test
-    public void testDatePicker() {
-        var dateMenuPage = homePage.goToWidgets().clickSelectMenu();
-        dateMenuPage.clickDatePicker().selectMonth("July");
-        dateMenuPage.clickDatePicker().selectYear("2025");
-        dateMenuPage.clickDatePicker().clickDay("11");
-        dateMenuPage.clickDatePicker().clickSelectedDate();
+    public void testSelectingDate() {
 
+        String month = "December";
+        String monthNumber = "12";
+
+        String day = "31";
+        String year = "2034";
+
+        var datePickerPage = homePage.goToWidgets().clickDatePicker();
+        datePickerPage.clickSelectedDate();
+        datePickerPage.selectMonth(month);
+        datePickerPage.selectYear(year);
+        datePickerPage.clickDay(day);
+
+        String actualDate = datePickerPage.getDate();
+        String expectedDate = monthNumber + "/" + day + "/" + year;
+        Assert.assertEquals(actualDate, expectedDate, "/n Actual & Expected Dates Do Not Match" +
+                                   "/n Actual Date:   " + actualDate +
+                                   "/n Expected Date: " + expectedDate);
     }
 }
